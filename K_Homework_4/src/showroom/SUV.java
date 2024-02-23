ff0df510919fddf39e47980932f14408a96156ed
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-23 14:42:14 -0600

    change the maintenace to 0.001 and fuel effieceny 0.05

--- K_Homework_4/src/showroom/SUV.java
+++ K_Homework_4/src/showroom/SUV.java
@@ -9,12 +9,12 @@ class SUV extends Vehicle {
 
     @Override
 	public double calculateMaintenaceCost(double distance) {
-        return distance * mass * (2024 - modelYear) * cylinders * 0.0005;
+        return distance * mass * (2024 - modelYear) * cylinders * 0.001;
     }
     
     @Override
     public double calculateFuelEfficiency(double distance, double fuelPrice){
-        return cylinders * gasTankCapacity * fuelPrice / distance * 0.003;
+        return cylinders * gasTankCapacity * fuelPrice / distance * 0.05;
     }
 
     @Override
